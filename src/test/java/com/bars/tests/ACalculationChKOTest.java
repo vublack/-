76760ff096e0abee6e494ffe042aadcc298dc68d
vasyl.chornyi <vublack@gmail.com>
2018-11-27 11:26:33 +0200

    БМД - Розрахунок ЧКО(Овердрафт.Ліміти)

--- src/test/java/com/bars/tests/ACalculationChKOTest.java
+++ src/test/java/com/bars/tests/ACalculationChKOTest.java
@@ -1,9 +1,11 @@
 package com.bars.tests;
 
 import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.junit.ScreenShooter;
 import com.codeborne.selenide.junit.TextReport;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
 import org.junit.BeforeClass;
@@ -21,6 +23,7 @@ import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
+
 public class ACalculationChKOTest {
     @Rule
     public ScreenShooter screenShooter = ScreenShooter.failedTests().to("test-results/reports");
@@ -29,17 +32,25 @@ public class ACalculationChKOTest {
 
     @BeforeClass
     public static void setup() {
-        timeout = 40000;
-//        baseUrl = "http://10.10.17.22:8080/barsroot";
-        baseUrl = "http://10.10.17.50:8080/barsroot/account/login/";
-//        baseUrl = "http://10.10.17.40:8080/barsroot/account/login/";
-//        baseUrl = "http://10.10.17.40:8082/barsroot/account/login/";
+        timeout = 80000;
+//        baseUrl = "http://10.10.17.22:8080/barsroot/account/login";
+//        baseUrl = "http://10.10.17.24:8080/barsroot/account/login";
+//        baseUrl = "http://10.10.17.50:8080/barsroot/account/login";
+//        baseUrl = "http://10.10.17.40:8080/barsroot/account/login";
+//        baseUrl = "http://10.10.17.40:8082/barsroot/account/login";
+//        baseUrl = "http://10.10.10.198:11111/barsroot/account/login";
+//        browser = "chrome";
         browser = "ie";
+        startMaximized = true;
 //        System.setProperty("webdriver.ie.driver", ".\\IEDriverServer.exe");
         InternetExplorerDriverManager.getInstance(DriverManagerType.IEXPLORER).arch32().setup();
+//        ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
         open("/");
+
     }
     @Test
+
+
     public void ACalculationChKO() {
         $("#txtUserName").setValue("absadm01");
         $("#txtPassword").setValue("qwerty").pressEnter();
